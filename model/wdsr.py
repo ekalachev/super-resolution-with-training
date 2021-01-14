006bb94d00ffc044e8f710827e43e4e04aa7d821
Eldar Kalachev <rusgunter@gmail.com>
2021-01-14 18:00:25 +0300

    removed extra shell for Conv2D

--- model/wdsr.py
+++ model/wdsr.py
@@ -1,5 +1,3 @@
-import tensorflow_addons as tfa
-
 from tensorflow.python.keras.layers import Add, Conv2D, Input, Lambda
 from tensorflow.python.keras.models import Model
 
@@ -19,14 +17,14 @@ def wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scal
     x = Lambda(normalize)(x_in)
 
     # main branch
-    m = conv2d_weightnorm(num_filters, 3, padding='same')(x)
+    m = Conv2D(num_filters, 3, padding='same')(x)
     for i in range(num_res_blocks):
         m = res_block(m, num_filters, res_block_expansion, kernel_size=3, scaling=res_block_scaling)
-    m = conv2d_weightnorm(3 * scale ** 2, 3, padding='same', name=f'conv2d_main_scale_{scale}')(m)
+    m = Conv2D(3 * scale ** 2, 3, padding='same', name=f'conv2d_main_scale_{scale}')(m)
     m = Lambda(pixel_shuffle(scale))(m)
 
     # skip branch
-    s = conv2d_weightnorm(3 * scale ** 2, 5, padding='same', name=f'conv2d_skip_scale_{scale}')(x)
+    s = Conv2D(3 * scale ** 2, 5, padding='same', name=f'conv2d_skip_scale_{scale}')(x)
     s = Lambda(pixel_shuffle(scale))(s)
 
     x = Add()([m, s])
@@ -36,8 +34,8 @@ def wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scal
 
 
 def res_block_a(x_in, num_filters, expansion, kernel_size, scaling):
-    x = conv2d_weightnorm(num_filters * expansion, kernel_size, padding='same', activation='relu')(x_in)
-    x = conv2d_weightnorm(num_filters, kernel_size, padding='same')(x)
+    x = Conv2D(num_filters * expansion, kernel_size, padding='same', activation='relu')(x_in)
+    x = Conv2D(num_filters, kernel_size, padding='same')(x)
     if scaling:
         x = Lambda(lambda t: t * scaling)(x)
     x = Add()([x_in, x])
@@ -46,14 +44,10 @@ def res_block_a(x_in, num_filters, expansion, kernel_size, scaling):
 
 def res_block_b(x_in, num_filters, expansion, kernel_size, scaling):
     linear = 0.8
-    x = conv2d_weightnorm(num_filters * expansion, 1, padding='same', activation='relu')(x_in)
-    x = conv2d_weightnorm(int(num_filters * linear), 1, padding='same')(x)
-    x = conv2d_weightnorm(num_filters, kernel_size, padding='same')(x)
+    x = Conv2D(num_filters * expansion, 1, padding='same', activation='relu')(x_in)
+    x = Conv2D(int(num_filters * linear), 1, padding='same')(x)
+    x = Conv2D(num_filters, kernel_size, padding='same')(x)
     if scaling:
         x = Lambda(lambda t: t * scaling)(x)
     x = Add()([x_in, x])
     return x
-
-
-def conv2d_weightnorm(filters, kernel_size, padding='same', activation=None, **kwargs):
-    return tfa.layers.WeightNormalization(Conv2D(filters, kernel_size, padding=padding, activation=activation, **kwargs), data_init=False)
